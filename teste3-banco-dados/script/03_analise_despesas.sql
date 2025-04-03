f2c2b49cee2fc63da32202ffe0d28f9c6ead527f
Leonardo Giampaglia Gomes <129779553+Giampaglia@users.noreply.github.com>
2025-04-02 22:30:40 -0300

    comit

--- teste3-banco-dados/script/03_analise_despesas.sql
+++ teste3-banco-dados/script/03_analise_despesas.sql
@@ -1,18 +1,19 @@
-SELECT o.nome_fantasia, SUM(dc.vl_saldo_final - dc.vl_saldo_inicial) AS total_despesas
+-- Últimos 3 meses
+SELECT o.razao_social, SUM(dc.vl_saldo_final - dc.vl_saldo_inicial) AS total_despesas
 FROM demonstracoes_contabeis dc
 JOIN operadoras o ON dc.id_operadora = o.id_operadora
 WHERE dc.data >= DATE_SUB(CURDATE(), INTERVAL 3 MONTH)
 AND dc.cd_conta_contabil = 'EVENTOS/SINISTROS'
-GROUP BY o.nome_fantasia
+GROUP BY o.razao_social
 ORDER BY total_despesas DESC
 LIMIT 10;
 
 -- Último ano
-SELECT o.nome_fantasia, SUM(dc.vl_saldo_final - dc.vl_saldo_inicial) AS total_despesas
+SELECT o.razao_social, SUM(dc.vl_saldo_final - dc.vl_saldo_inicial) AS total_despesas
 FROM demonstracoes_contabeis dc
 JOIN operadoras o ON dc.id_operadora = o.id_operadora
 WHERE YEAR(dc.data) = YEAR(CURDATE()) - 1
 AND dc.cd_conta_contabil = 'EVENTOS/SINISTROS'
-GROUP BY o.nome_fantasia
+GROUP BY o.razao_social
 ORDER BY total_despesas DESC
 LIMIT 10;
